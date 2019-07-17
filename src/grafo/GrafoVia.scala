36f76539fa5ced5c01ad67dc99f61ce85187c1da
Alejo <alortizme@unal.edu.co>
2019-07-17 13:03:29 -0500

    Correción en GrafoVia.

--- src/grafo/GrafoVia.scala
+++ src/grafo/GrafoVia.scala
@@ -16,8 +16,8 @@ object GrafoVia {
     val intersecciones: Array[Interseccion] = vias.map(via => via.origen) ++ vias.map(via => via.fin)
 
     intersecciones.foreach(interseccion => grafo.add(interseccion))
-    vias.foreach(via => grafo.add(WLUnDiEdge(via.origen, via.fin)(via.distancia, via.nombre)))
-    grafo.add
+    vias.foreach(via => grafo.add(WLUnDiEdge(via.origen, via.fin)(via.longitud, via.nombre)))
+    /* Código sin terminar */
   }
 
 }
