9f8eb893392ff2ad7ea31d162d66a1cf2d74e996
Nicolás <ncapdetm@unal.edu.co>
2019-08-26 19:27:55 -0500

    Correción en la clase Velocidad Se debe pasar la magnitud de la velocidad a metros por segundo para que sea coherente con los cálculos hechos usando la aceleración.

--- src/fisica/Velocidad.scala
+++ src/fisica/Velocidad.scala
@@ -4,6 +4,7 @@ import scala.math.{cos, sin, toRadians}
 import geometria.Angulo
 
 case class Velocidad(private var _magnitud: Double, private var _direccion: Angulo) {
+
   def magnitud_=(magnitud: Double): Unit = _magnitud = magnitud
   def magnitud: Double = _magnitud
 
@@ -12,9 +13,11 @@ case class Velocidad(private var _magnitud: Double, private var _direccion: Angu
 
   def velocidadDireccionX(): Double = cos(toRadians(direccion.grados)) * magnitud
   def velocidadDireccionY(): Double = sin(toRadians(direccion.grados)) * magnitud
+
+  magnitud = Velocidad.aMetrosPorSegundo(_magnitud)
 }
 
 object Velocidad {
-  def aKilometrosPorHora(metrosPorSegundo: Double): Double = metrosPorSegundo * (18/5)
-  def aMetrosPorSegundo(kilometrosPorHora: Double): Double = kilometrosPorHora * (5/18)
+  def aKilometrosPorHora(metrosPorSegundo: Double): Double = metrosPorSegundo * (18.0/5.0)
+  def aMetrosPorSegundo(kilometrosPorHora: Double): Double = kilometrosPorHora * (5.0/18.0)
 }
