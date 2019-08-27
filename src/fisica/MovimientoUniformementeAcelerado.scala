e5152b59c49665815da4d17e6c5fb5c86a832c6c
Nicolás <ncapdetm@unal.edu.co>
2019-08-27 15:39:20 -0500

    Trait MovimientoUniformementeAcelerado terminado

--- src/fisica/MovimientoUniformementeAcelerado.scala
+++ src/fisica/MovimientoUniformementeAcelerado.scala
@@ -7,13 +7,19 @@ trait MovimientoUniformementeAcelerado {
   def movimientoUniformementeAcelerado(dt: Double,
                                        posicion: Punto,
                                        velocidad: Velocidad,
-                                       aceleracion: Double = 2): Punto = {
+                                       aceleracion: Double): Punto = {
 
     val nuevaX: Double = (aceleracion * Math.cos(Math.toRadians(velocidad.direccion.grados)) * Math.pow(dt, 2.0) / 2.0) + velocidad.velocidadDireccionX() * dt + posicion.x
     val nuevaY: Double = (aceleracion * Math.sin(Math.toRadians(velocidad.direccion.grados)) * Math.pow(dt, 2.0) / 2.0) + velocidad.velocidadDireccionY() * dt + posicion.y
 
-    velocidad.magnitud = Math.hypot(Math.abs(velocidad.velocidadDireccionX()) + aceleracion * dt, Math.abs(velocidad.velocidadDireccionY()) + aceleracion * dt)
+    velocidad.magnitud = velocidadUniformementeAcelerada(dt, velocidad, aceleracion)
 
     Punto(nuevaX, nuevaY)
+  }
+
+  def velocidadUniformementeAcelerada(dt: Double,
+                                      velocidad: Velocidad,
+                                      aceleracion: Double): Double = {
+    velocidad.magnitud + aceleracion * dt
   }
 }
